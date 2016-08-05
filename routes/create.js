b1bf4cac3387f29e37a81ac9dfbc33e836275046
necoline <hnhubner@gmail.com>
2016-08-04 21:01:33 -0600

    create and robots

--- routes/create.js
+++ routes/create.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis');
+var redisClient = redis.createClient();
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('create', { title: 'Create' });
+});
+
+router.post('/', function(req, res) {
+  redisClient.sadd("robots", req.body.robotName);
+
+
+
+  
+  res.redirect('/robots');
 });
 
 module.exports = router;
--- routes/robots.js
+++ routes/robots.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis');
+var redisClient = redis.createClient();
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('robots', { title: 'Robots' });
+  redisClient.smembers("robots", function( err, robotData) {
+    res.locals.robotArray = robotData;
+    res.render('robots', { title: 'Robots' });
+  });
 });
 
 module.exports = router;
